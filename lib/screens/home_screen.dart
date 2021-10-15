c457a3d46c2f0b975a3d90adc7a6f597ba081bf2
mizuhomai <choco.hana.choco@gmail.com>
2021-10-15 23:59:26 +0900

    yu

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -13,51 +13,45 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         backgroundColor: Color(0xFF021524),
         leading: Padding(
-          padding: EdgeInsets.only(left:2.0),
+          padding: EdgeInsets.only(left: 2.0),
           child: InkResponse(
             onTap: () => print('Menu'),
-            child: Icon(
-                Icons.menu,
-                size:36.0,
-                color: Colors.white
-            ),
+            child: Icon(Icons.menu, size: 36.0, color: Colors.white),
           ),
         ),
         title: Image(
-          image:AssetImage('assets/images/amazon_logo.png'),
+          image: AssetImage('assets/images/amazon_logo.png'),
           height: 40.0,
         ),
         actions: <Widget>[
-          TextButton(onPressed: (){}, child: Text('ログイン',style: TextStyle(color: Colors.white),)),
+          TextButton(
+              onPressed: () {},
+              child: Text(
+                'ログイン',
+                style: TextStyle(color: Colors.white),
+              )),
           Padding(
-            padding: EdgeInsets.only(right:20.0),
+            padding: EdgeInsets.only(right: 20.0),
             child: InkResponse(
               onTap: () => print('ログイン'),
-              child: Icon(
-                  Icons.person_outline,
-                  size:40.0,
-                  color: Colors.white
-              ),
+              child:
+                  Icon(Icons.person_outline, size: 40.0, color: Colors.white),
             ),
           ),
-
           Stack(
             children: [
               Padding(
-                padding: EdgeInsets.only(top:11,right:14.0),
+                padding: EdgeInsets.only(top: 11, right: 14.0),
                 child: InkResponse(
                   onTap: () => print('カート'),
-                  child: Icon(
-                      Icons.shopping_cart_outlined,
-                      size:36.0,
-                      color: Colors.white
-                  ),
+                  child: Icon(Icons.shopping_cart_outlined,
+                      size: 36.0, color: Colors.white),
                 ),
               ),
               Positioned(
-                  bottom: 34.0,
-                  right: 18.0,
-                  child: Container(
+                bottom: 34.0,
+                right: 17.0,
+                child: Container(
                     height: 20.0,
                     width: 20.0,
                     decoration: BoxDecoration(
@@ -71,15 +65,59 @@ class _HomeScreenState extends State<HomeScreen> {
                         fontSize: 20,
                         fontWeight: FontWeight.bold,
                       ),
-                    )
-                  ),
+                    ),
+                ),
               ),
             ],
-          )
-
-
+          ),
         ],
       ),
-    );
+
+      body: ListView(
+
+        children: <Widget>[
+
+            Stack(
+                children: <Widget>[
+
+                  Container(
+                    height: 60,
+                    color:Color(0xFF021524),
+
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8.0),
+                        color: Colors.white,
+                      ),
+                      margin: const EdgeInsets.symmetric(horizontal: 2,vertical: 8),
+                      child: TextField(),
+                    ),
+                  ),
+
+                  Positioned(
+                    bottom: 8.0,
+                    right: 2.0,
+                    child: Container(
+                      width: 44,
+                      height: 44,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8.0),
+                        color: Colors.orange,),
+
+                        child: Icon(Icons.search),
+                    ),
+                  ),
+                ],
+            ),
+          Container(
+            height: 40,
+            color:Colors.blueGrey[900],
+          ),
+          Image(image: AssetImage('assets/images/amazon_top.jpg'),
+          ),
+
+
+
+    ]));
   }
 }
